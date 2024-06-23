cfc7f83a8b42fa4a88d89376c87c84c586d39699
Waishnav <waishnavdeore@gmail.com>
2024-06-24 00:04:24 +0530

    added js for dropdown, and for markdown editor in future

--- lib/simple_discussion/engine.rb
+++ lib/simple_discussion/engine.rb
@@ -6,5 +6,24 @@ module SimpleDiscussion
     config.after_initialize do
       SimpleDiscussion::Engine.routes.default_url_options = ActionMailer::Base.default_url_options
     end
+
+    # javascripts assets precompiled for dropdown and markdown text editor
+    @@javascripts = []
+
+    initializer "simple_discussion.assets.precompile" do |app|
+      app.config.assets.precompile += [
+        "simple_discussion/application.js",
+      ]
+    end
+
+    def self.add_javascript(script)
+      @@javascripts << script
+    end
+
+    def self.javascripts
+      @@javascripts
+    end
+
+    add_javascript "simple_discussion/application.js"
   end
 end
