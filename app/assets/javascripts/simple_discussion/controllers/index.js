99d7ab59b350e17158f03a6723bf0081867c6797
Waishnav <waishnavdeore@gmail.com>
2024-07-07 17:58:05 +0530

    feat: adding markdown editor using simplemde

--- app/assets/javascripts/simple_discussion/controllers/index.js
+++ app/assets/javascripts/simple_discussion/controllers/index.js
@@ -2,6 +2,8 @@ import { application } from "./application"
 
 import DropdownController from "./dropdown_controller"
 import ReportSpamController from "./report_spam_controller";
+import SimplemdeController from "./simplemde_controller";
 
 application.register("dropdown", DropdownController);
 application.register("report-spam", ReportSpamController);
+application.register("simplemde",SimplemdeController);
