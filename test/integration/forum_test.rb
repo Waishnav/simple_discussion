f6977ee2be9387a7f750455103fde07cfd092b3b
Waishnav <waishnavdeore@gmail.com>
2024-07-21 14:49:44 +0530

    added tests for profanity filters

--- test/integration/forum_test.rb
+++ test/integration/forum_test.rb
@@ -6,6 +6,7 @@ class ForumTest < ActionDispatch::IntegrationTest
 
   setup do
     sign_in users(:one)
+    @filter = LanguageFilter::Filter.new
   end
 
   test "threads index" do
@@ -52,5 +53,77 @@ class ForumTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to forum_thread_path(forum_threads(:hello), anchor: dom_id(ForumPost.last))
+  end
+
+  test "cannot create a forum thread with inappropriate language in title" do
+    inappropriate_word = @filter.matchlist.to_a.sample
+    assert_no_difference "ForumThread.count" do
+      assert_no_difference "ForumPost.count" do
+        post forum_threads_path, params: {
+          forum_thread: {
+            forum_category_id: forum_categories(:general).id,
+            title: "This title contains inappropriate language: #{inappropriate_word}",
+            forum_posts_attributes: [{
+              body: "Clean body"
+            }]
+          }
+        }
+      end
+    end
+
+    assert_response :unprocessable_entity
+    assert_includes response.body, "Title contains inappropriate language"
+  end
+
+  test "cannot create a forum thread with inappropriate language in body" do
+    inappropriate_word = @filter.matchlist.to_a.sample
+    assert_no_difference "ForumThread.count" do
+      assert_no_difference "ForumPost.count" do
+        post forum_threads_path, params: {
+          forum_thread: {
+            forum_category_id: forum_categories(:general).id,
+            title: "Clean Title",
+            forum_posts_attributes: [{
+              body: "This post contains inappropriate language: #{inappropriate_word}"
+            }]
+          }
+        }
+      end
+    end
+
+    assert_response :unprocessable_entity
+    assert_includes response.body, "Body contains inappropriate language"
+  end
+
+  test "cannot reply to a forum thread with inappropriate language" do
+    inappropriate_word = @filter.matchlist.to_a.sample
+    assert_no_difference "ForumPost.count" do
+      post forum_thread_forum_posts_path(forum_threads(:hello)), params: {
+        forum_post: {
+          body: "This reply contains inappropriate language: #{inappropriate_word}"
+        }
+      }
+    end
+
+    assert_response :unprocessable_entity
+    assert_includes response.body, "Body contains inappropriate language"
+  end
+
+  test "can create a forum thread with appropriate language in title and body" do
+    assert_difference "ForumThread.count" do
+      assert_difference "ForumPost.count" do
+        post forum_threads_path, params: {
+          forum_thread: {
+            forum_category_id: forum_categories(:general).id,
+            title: "Clean Thread Title",
+            forum_posts_attributes: [{
+              body: "This is a clean and appropriate post."
+            }]
+          }
+        }
+      end
+    end
+
+    assert_redirected_to forum_thread_path(ForumThread.last)
   end
 end
