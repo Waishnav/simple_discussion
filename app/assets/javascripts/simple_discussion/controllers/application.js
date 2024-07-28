99e9d8ded8e0f9f3cbff2bf6cbefaa2f375b2fbd
Waishnav <waishnavdeore@gmail.com>
2024-07-28 22:32:25 +0530

    removing stimulus controller

--- app/assets/javascripts/simple_discussion/controllers/application.js
+++
@@ -1,9 +0,0 @@
-import { Application } from "@hotwired/stimulus"
-
-const application = Application.start()
-
-application.warnings = true
-application.debug    = false
-window.Stimulus      = application
-
-export { application }
--- app/assets/javascripts/simple_discussion/controllers/index.js
+++
@@ -1,7 +0,0 @@
-import { application } from "./application"
-
-import DropdownController from "./dropdown_controller"
-import ReportSpamController from "./report_spam_controller";
-
-application.register("dropdown", DropdownController);
-application.register("report-spam", ReportSpamController);
