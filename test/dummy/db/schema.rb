82b5c4b46b210985cc9380b8f2e572542aa98107
Waishnav <waishnavdeore@gmail.com>
2024-08-20 23:02:22 +0530

    updated schema

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -68,17 +68,6 @@ ActiveRecord::Schema.define(version: 2024_08_13_072347) do
     t.index ["user_id"], name: "index_spam_reports_on_user_id"
   end
 
-  create_table "spam_reports", force: :cascade do |t|
-    t.integer "forum_post_id", null: false
-    t.integer "user_id", null: false
-    t.integer "reason", null: false
-    t.text "details"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["forum_post_id"], name: "index_spam_reports_on_forum_post_id"
-    t.index ["user_id"], name: "index_spam_reports_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
