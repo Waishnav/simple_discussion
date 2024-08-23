75955b629fb097dd2db6fa977dcfb77ffa3cebcb
Waishnav <waishnavdeore@gmail.com>
2024-08-23 20:50:17 +0530

    fix: standardrb

--- app/helpers/simple_discussion/forum_threads_helper.rb
+++ app/helpers/simple_discussion/forum_threads_helper.rb
@@ -26,7 +26,7 @@ module SimpleDiscussion::ForumThreadsHelper
     self.output_buffer = ActionView::OutputBuffer.new(output)
   end
 
-# simple sql query implementation of basic_search, this helper method can be complex as it you wanted it to be
+  # simple sql query implementation of basic_search, this helper method can be complex as it you wanted it to be
   def topic_search(query)
     ForumThread.joins(:forum_posts)
       .where("forum_threads.title LIKE :query OR forum_posts.body LIKE :query", query: "%#{query}%")
